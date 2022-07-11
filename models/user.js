742fa1937ad0653e0f08ec255f12bc938110233c
lshyun955 <lshyun955@gmail.com>
2022-07-11 18:02:27 +0900

    Add: 회원가입 기능

--- models/user.js
+++ models/user.js
@@ -1,2 +1,15 @@
 import prismaClient from './prisma-client.js';
-export const createUser = async (email, password) => {};
+export const createUser = async (email, password, nickname, name) => {
+  return await prismaClient.users.create({
+    data: {
+      email,
+      password,
+      nickname,
+      name,
+    },
+  });
+};
+
+export const readUserByEmail = async email => {
+  return await prismaClient.users.findUnique({ where: { email } });
+};
--- services/user.js
+++ services/user.js
@@ -2,4 +2,10 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import * as userRepository from '../models/user.js';
 
-export const signUp = async (email, password) => {};
+export const signUp = async query => {
+  const { email, password } = query;
+  const nickname = query.nickname ? query.nickname : query.email.split('@')[0];
+  const name = query.name ? query.name : query.email.split('@')[0];
+
+  const [user] = await userRepository.readUserByEmail(email);
+};
