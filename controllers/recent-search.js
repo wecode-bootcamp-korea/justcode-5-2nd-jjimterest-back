2fb06071a92f5035d190d882c0a061f4b8929895
azure928 <azure6.dev@gmail.com>
2022-07-13 00:43:31 +0900

    Add : GET  최근 검색 기록 키워드

--- controllers/recent-search.js
+++ controllers/recent-search.js
@@ -3,7 +3,9 @@ import * as recentSearchService from '../services/recent-search.js';
 export const serchList = async (req, res) => {
   try {
     let { id } = req.params;
+    console.log('id : ', id);
     const keywords = await recentSearchService.serchList(id);
+    console.log('recentSearchController - keywords : ', keywords);
     res.status(200).json(keywords);
   } catch (error) {
     console.log(err);
--- services/recent-search.js
+++ services/recent-search.js
@@ -2,5 +2,6 @@ import * as recentSearchModels from '../models/recent-search.js';
 
 export async function serchList() {
   const keywords = await recentSearchModels.serchList();
+  console.log('recentSearchService - keywords : ', keywords);
   return keywords;
 }
