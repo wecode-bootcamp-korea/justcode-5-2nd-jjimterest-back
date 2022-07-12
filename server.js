6c522efafbc1b0a69ff65fd43800095fbb2eb3f0
azure928 <azure6.dev@gmail.com>
2022-07-12 21:47:02 +0900

    Modify : 피어 리뷰 반영

--- server.js
+++ server.js
@@ -19,9 +19,3 @@ const PORT = process.env.PORT || 10010;
 app.listen(PORT, () => {
   console.log(`server start PORT:${PORT}`);
 });
-
-////////// TEST ///////////////
-app.get('/ping', (req, res) => {
-  res.json({ message: 'pong' });
-});
-///////////////////////////////
