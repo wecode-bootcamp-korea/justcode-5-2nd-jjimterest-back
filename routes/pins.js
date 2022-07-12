fc0504406d9b98c7711e1e81782485180247cfee
azure928 <azure6.dev@gmail.com>
2022-07-12 20:24:41 +0900

    Add : pin 목록 - auth 미들웨어 가져다 쓰는 것 추가

--- routes/pins.js
+++ routes/pins.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import * as pinsController from '../controllers/pins.js';
+//import { isLogin } from '../middleware/auth.js';
 const router = express.Router();
+
+//router.use(isLogin);
 
 router.get('/pins', pinsController.pinList);
 
