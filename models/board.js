9f07d59de745207e224e6a24d4e254098d1e8378
lshyun955 <lshyun955@gmail.com>
2022-07-12 21:26:19 +0900

    Add: 보드 삭제 기능 구현

--- models/board.js
+++ models/board.js
@@ -8,3 +8,13 @@ export const createBoard = async (userId, title) => {
     },
   });
 };
+
+export const readBoardById = async board_id => {
+  return await prismaClient.boards.findUnique({
+    where: { id: Number(board_id) },
+  });
+};
+
+export const deleteBoard = async board_id => {
+  return await prismaClient.boards.delete({ where: { id: Number(board_id) } });
+};
