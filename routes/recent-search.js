885386c5a254b7b937ebaa7f76873f004f3fec61
azure928 <azure6.dev@gmail.com>
2022-07-13 01:41:19 +0900

    Modify

--- routes/recent-search.js
+++ routes/recent-search.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import * as recentSearchController from '../controllers/recent-search.js';
-//import { isLogin } from '../middleware/auth.js';
+import { isLogin } from '../middleware/auth.js';
 const router = express.Router();
 
-//router.use(isLogin);
+router.use(isLogin);
 
 router.get('/recent-search/:id', recentSearchController.serchList);
 
