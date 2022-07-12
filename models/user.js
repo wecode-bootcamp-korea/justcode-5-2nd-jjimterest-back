8519149da3a6f0435c9f4d029c5dd21d65a1155a
lshyun955 <lshyun955@gmail.com>
2022-07-12 17:13:36 +0900

    Fix: 카카오 소셜 로그인 social_login 테이블 추가 후 구현

--- models/user.js
+++ models/user.js
@@ -18,6 +18,21 @@ export const createUser = async (
   });
 };
 
+export const createSocialUser = async (id, userId) => {
+  return await prismaClient.social_login.create({
+    data: {
+      social_id: String(id),
+      user_id: userId,
+    },
+  });
+};
+
 export const readUserByEmail = async email => {
   return await prismaClient.users.findUnique({ where: { email } });
+};
+
+export const readUserBySocialId = async id => {
+  return await prismaClient.social_login.findFirst({
+    where: { social_id: String(id) },
+  });
 };
