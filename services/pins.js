fc73773090e0c8a8d50dabff4d86f1934cfaaeab
lshyun955 <lshyun955@gmail.com>
2022-07-13 16:09:07 +0900

    Add: 핀 상세보기 기능 구현

--- services/pins.js
+++ services/pins.js
@@ -4,3 +4,13 @@ export async function pinList() {
   const pins = await pinsModels.pinList();
   return pins;
 }
+
+export const readPinById = async (pinId, userId) => {
+  const pin = await pinsModels.readPinById(pinId, userId);
+  if (!!!pin.length) {
+    const error = new Error('해당 핀이 존재하지 않습니다.');
+    error.statusCode = 404;
+    throw error;
+  }
+  return pin;
+};
