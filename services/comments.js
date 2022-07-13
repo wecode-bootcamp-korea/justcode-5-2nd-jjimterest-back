311b8219bac1921c5962c46f52a23fdd3230c88a
lshyun955 <lshyun955@gmail.com>
2022-07-13 22:06:42 +0900

    Add: 댓글 수정 기능 구현

--- services/comments.js
+++ services/comments.js
@@ -15,3 +15,19 @@ export const createComment = async (userId, parentId, pinId, content) => {
     content
   );
 };
+
+export const updateComment = async (userId, commentId, content) => {
+  const comment = await commentsRepository.readCommentsById(commentId);
+  if (!Boolean(comment.length)) {
+    const error = new Error('해당 댓글이 존재하지 않습니다.');
+    error.statusCode = 404;
+    throw error;
+  }
+  if (userId !== comment[0].user_id) {
+    const error = new Error('해당 댓글을 수정할 권한이 없습니다.');
+    error.statusCode = 400;
+    throw error;
+  }
+
+  await commentsRepository.updateComment(commentId, content);
+};
