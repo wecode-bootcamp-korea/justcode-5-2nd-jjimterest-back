794bd9f103ffe42b43911d54f4c5593c814edba7
azure928 <azure6.dev@gmail.com>
2022-07-13 13:30:56 +0900

    Modify

--- services/recent-search.js
+++ services/recent-search.js
@@ -2,6 +2,5 @@ import * as recentSearchModels from '../models/recent-search.js';
 
 export async function serchList(id) {
   const keywords = await recentSearchModels.serchList(id);
-  console.log('recentSearchService - keywords : ', keywords);
   return keywords;
 }
