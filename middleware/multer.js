ffaff549ae2388d60bfec08f2cfae911f27cb420
azure928 <azure6.dev@gmail.com>
2022-07-18 00:30:42 +0900

    Modify : multer를 middleware로 분리함

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,14 +1,5 @@
 import multer from 'multer';
 import path from 'path';
-import fs from 'fs';
-
-//uploads 폴더가 없을시 생성
-try {
-  fs.readdirSync('uploads');
-} catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-  fs.mkdirSync('uploads');
-}
 
 export const upload = multer({
   storage: multer.diskStorage({
