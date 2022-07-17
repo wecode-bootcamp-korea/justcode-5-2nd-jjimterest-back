149868a7153b958c2e12657225d61037518422d3
azure928 <azure6.dev@gmail.com>
2022-07-18 00:13:46 +0900

    Modify : multer를 middleware로 분리함 - pins routes에 있던 multer 관련 내용을 middleware로 분리하였음

---
+++ middleware/multer.js
@@ -0,0 +1,23 @@
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+
+//uploads 폴더가 없을시 생성
+try {
+  fs.readdirSync('uploads');
+} catch (error) {
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
+}
+
+export const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+});
