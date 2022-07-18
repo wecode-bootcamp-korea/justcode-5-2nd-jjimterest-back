f25c9f972462c3a4c9c11dcb55694cbedcbcf06f
azure928 <azure6.dev@gmail.com>
2022-07-18 20:51:35 +0900

    Add : 마이페이 불러오는 기능

--- controllers/mypage.js
+++ controllers/mypage.js
@@ -1,11 +1,22 @@
 import * as myPageService from '../services/mypage.js';
 
-export const readMyPage = async (req, res) => {
+export const readEditProfile = async (req, res) => {
   try {
     const userId = req.userId;
 
-    const mypage = await myPageService.readMyPage();
-    res.status(200).json(pins);
+    const mypage = await myPageService.readEditProfile(userId);
+    res.status(200).json(mypage);
+  } catch (error) {
+    res.status(error.statusCode || 500).json({ message: error.message });
+  }
+};
+
+export const readAccountSettings = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const accountSettings = await myPageService.readAccountSettings(userId);
+    res.status(200).json(accountSettings);
   } catch (error) {
     res.status(error.statusCode || 500).json({ message: error.message });
   }
--- services/mypage.js
+++ services/mypage.js
@@ -1,7 +1,13 @@
 import * as myPageModels from '../models/mypage.js';
 
-export async function readMyPage(userId) {
-  const mypage = await myPageModels.readMyPage(userId);
+export async function readEditProfile(userId) {
+  const mypage = await myPageModels.readEditProfile(userId);
 
   return mypage;
+}
+
+export async function readAccountSettings(userId) {
+  const accountSettings = await myPageModels.readAccountSettings(userId);
+
+  return accountSettings;
 }
