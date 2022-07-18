229d7eab134b30498a8474af6ee2ec9bbbf1cf76
azure928 <azure6.dev@gmail.com>
2022-07-18 20:51:35 +0900

    Add : 마이페이 불러오는 기능

--- models/mypage.js
+++ models/mypage.js
@@ -1,9 +1,21 @@
 import prismaClient from './prisma-client.js';
 
-export async function readMyPage(keyword, pageNumber) {
+export async function readEditProfile(userId) {
   const mypage = await prismaClient.$queryRawUnsafe(`
-  
+  select profile_image, name, intro, nickname
+  from users
+  where id = ${userId};
   `);
 
   return mypage;
+}
+
+export async function readAccountSettings(userId) {
+  const accountSettings = await prismaClient.$queryRawUnsafe(`
+  select email, password
+  from users
+  where id = ${userId};
+  `);
+
+  return accountSettings;
 }
