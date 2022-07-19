0ba490d8be3ee12b96c5b459a8bc9d99c3d84768
azure928 <azure6.dev@gmail.com>
2022-07-20 02:35:49 +0900

    Add : 프로필 수정 기능 - 이름, 소개, 닉네임 수정 - 프로필 사진 수정 기능 추가 예정

--- routes/mypage.js
+++ routes/mypage.js
@@ -4,10 +4,11 @@ import { isLogin } from '../middleware/auth.js';
 const router = express.Router();
 router.use(isLogin);
 
-// 공개 프로필 수정 페이지 (프로필 사진, 이름, 소개, 닉네임 수정하는 페이지)
+// 공개 프로필 수정 페이지 (프로필 사진, 이름, 소개, 닉네임 변경)
 router.get('/edit-profile', myPageController.readEditProfile);
+router.put('/edit-profile', myPageController.updateProfile);
 
-// 계정 관리 페이지 (비밀번호 수정하는 페이지)
+// 계정 관리 페이지 (비밀번호 변경)
 router.get('/account-settings', myPageController.readAccountSettings);
 router.put('/account-settings', myPageController.updatePassword);
 
