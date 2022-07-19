7fe33317ff84c632f3821615927d5a6066b31dbf
azure928 <azure6.dev@gmail.com>
2022-07-19 13:16:04 +0900

    Modify

--- models/pin-make.js
+++ models/pin-make.js
@@ -1,5 +1,6 @@
 import prismaClient from './prisma-client.js';
 
+/*
 export async function readMakePinPage(userId) {
   const makePinPage = await prismaClient.$queryRaw`
   SELECT boards.id as board_id, boards.title, boards.cover_image_url, users.name as user_name, COUNT(followings.follower_id) as follower_count
@@ -9,6 +10,18 @@ export async function readMakePinPage(userId) {
   WHERE boards.user_id = ${userId}
   GROUP BY boards.id;
   `;
+  return makePinPage;
+}*/
+
+export async function readMakePinPage(userId) {
+  const makePinPage = await prismaClient.$queryRaw`
+  SELECT user_id, users.name as user_name, COUNT(followings.follower_id) as follower_count,
+  JSON_ARRAYAGG(JSON_OBJECT("board_id", boards.id, "title", boards.title, "cover_image_url", boards.cover_image_url)) as boards
+  FROM boards
+  JOIN users on boards.user_id = users.id
+  JOIN followings on users.id = followee_id
+  WHERE boards.user_id = ${userId}
+  GROUP BY user_id`;
   return makePinPage;
 }
 
