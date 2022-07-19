0c93bd91228c82bc22b769af382e1ddb3493d5fa
lshyun955 <lshyun955@gmail.com>
2022-07-19 16:45:23 +0900

    Fix: 핀 상세보기 JOIN쿼리 수정

--- models/pins.js
+++ models/pins.js
@@ -20,11 +20,11 @@ export async function insertKeyword(keyword, userId) {
 export const readPinById = async (pinId, userId) => {
   const pin = await prismaClient.$queryRawUnsafe(`
   SELECT pins.id, user.nickname,user.count,pins.title, pins.intro, pins.image, comments.comments
-from pins join (SELECT
+from pins LEFT JOIN (SELECT
                     pin_id,
                     JSON_ARRAYAGG(CASE WHEN comments.id IS NOT NULL THEN JSON_OBJECT('id',comments.id,'content',comments.content,'parent_id',comments.parent_id,'profile_image',comment_user.profile_image,'user_id',comment_user.id,'created_at',comments.created_at,'like_count',cl.like_count ,'isLike',IFNULL((SELECT isLike from comment_like where user_id=${userId} and comment_like.comment_id=comments.id),0)) END) comments
                 FROM comments join (SELECT id,profile_image,nickname from users) comment_user on comments.user_id=comment_user.id join (SELECT comment_id,COUNT(*) like_count from comment_like where isLike=true group by comment_id) cl on comments.id=cl.comment_id where pin_id=${pinId}) comments on pins.id = comments.pin_id
-join (SELECT id, nickname,following.count from users join (SELECT followee_id,COUNT(*) count from followings  group by followee_id) following ON followee_id=id) user on pins.user_id=user.id
+LEFT JOIN (SELECT id, nickname,following.count from users join (SELECT followee_id,COUNT(*) count from followings  group by followee_id) following ON followee_id=id) user on pins.user_id=user.id
 where pins.id=${pinId}
   `);
   return pin;
