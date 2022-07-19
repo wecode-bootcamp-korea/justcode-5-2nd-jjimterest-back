c6ff797ee499f808885645c05363d8438bc276d7
azure928 <azure6.dev@gmail.com>
2022-07-19 19:49:20 +0900

    Add : 비밀번호 수정 기능 구현

--- controllers/mypage.js
+++ controllers/mypage.js
@@ -21,3 +21,20 @@ export const readAccountSettings = async (req, res) => {
     res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
+
+export const updatePassword = async (req, res) => {
+  try {
+    const { current_password, new_password, confirm_new_password } = req.body;
+    const userId = req.userId;
+    console.log(current_password, new_password, confirm_new_password, userId);
+    await myPageService.updatePassword(
+      userId,
+      current_password,
+      new_password,
+      confirm_new_password
+    );
+    res.status(200).json({ message: '비밀번호가 변경되었습니다.' });
+  } catch (error) {
+    res.status(error.statusCode || 500).json({ message: error.message });
+  }
+};
