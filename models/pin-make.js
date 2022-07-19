bd9641f504fd204d0c9db1866cd55811c87c03cd
azure928 <azure6.dev@gmail.com>
2022-07-19 16:43:32 +0900

    Fix : pin 만들기 화면 불러오는 쿼리 수정 - user_name과 follower_count가 board 개수만큼 중복 되지 않도록 쿼리 수정

--- models/pin-make.js
+++ models/pin-make.js
@@ -1,27 +1,16 @@
 import prismaClient from './prisma-client.js';
 
-/*
 export async function readMakePinPage(userId) {
   const makePinPage = await prismaClient.$queryRaw`
-  SELECT boards.id as board_id, boards.title, boards.cover_image_url, users.name as user_name, COUNT(followings.follower_id) as follower_count
-  FROM boards
-  JOIN users on boards.user_id = users.id
-  JOIN followings on users.id = followee_id
-  WHERE boards.user_id = ${userId}
-  GROUP BY boards.id;
+  select users.name, boards.follower_count,JSON_ARRAYAGG(JSON_OBJECT('board_id',boards.id,"title", boards.title,'cover_image_url',boards.cover_image_url)) boards
+  from users
+  LEFT JOIN (SELECT boards.id, boards.user_id, boards.title, boards.cover_image_url, boards.count follower_count
+  FROM (SELECT boards.id, boards.user_id, boards.title, boards.cover_image_url, followings.count FROM boards LEFT JOIN (SELECT followee_id, COUNT(*) count FROM followings group by followee_id) followings ON boards.user_id=followings.followee_id WHERE boards.user_id=${userId} group by boards.id) boards
+  GROUP BY boards.id) boards
+  ON users.id=boards.user_id
+  where users.id = ${userId}
+  GROUP BY users.name
   `;
-  return makePinPage;
-}*/
-
-export async function readMakePinPage(userId) {
-  const makePinPage = await prismaClient.$queryRaw`
-  SELECT user_id, users.name as user_name, COUNT(followings.follower_id) as follower_count,
-  JSON_ARRAYAGG(JSON_OBJECT("board_id", boards.id, "title", boards.title, "cover_image_url", boards.cover_image_url)) as boards
-  FROM boards
-  JOIN users on boards.user_id = users.id
-  JOIN followings on users.id = followee_id
-  WHERE boards.user_id = ${userId}
-  GROUP BY users.id, boards.id`;
   return makePinPage;
 }
 
