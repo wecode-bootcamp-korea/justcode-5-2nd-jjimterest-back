7158aaf8a517319e5d9716a1510a0847d7bccf56
lshyun955 <lshyun955@gmail.com>
2022-07-20 19:12:08 +0900

    Fix: 유저 프로필 정보 all_pins 부분 쿼리 수정

--- models/user.js
+++ models/user.js
@@ -54,13 +54,13 @@ export const getUserInfoByUserId = async (userId, otherUserId) => {
   (SELECT JSON_ARRAYAGG(JSON_OBJECT('id',pin_board.id,'title',pin_board.title,'pins',pin_board.pins)) boards_array FROM (SELECT boards.id, boards.title,JSON_ARRAYAGG(CASE WHEN pins.id IS NOT NULL THEN JSON_OBJECT('pin_id',pins.id,'image',pins.image) END) pins FROM boards LEFT JOIN board_store ON boards.id = board_store.board_id LEFT JOIN pins ON board_store.pin_id = pins.id WHERE boards.user_id=${
     otherUserId ? otherUserId : userId
   } GROUP BY boards.id) pin_board) boards,
-  (SELECT JSON_ARRAYAGG(JSON_OBJECT('id',p.pin_id,'image',p.image)) no_idea_pin FROM (SELECT pin_id, pins.image FROM unboard_pin JOIN pins ON unboard_pin.pin_id = pins.id WHERE unboard_pin.user_id=${userId}) p) no_idea_pins,
-  (SELECT JSON_MERGE(JSON_ARRAYAGG(JSON_OBJECT('id',p.pin_id,'image',image)), IFNULL((SELECT JSON_ARRAYAGG(JSON_OBJECT('id',pins.id,'image',image))
-FROM boards
-     JOIN board_store ON boards.id = board_store.board_id
-     JOIN (SELECT id,image FROM pins) pins ON board_store.pin_id = pins.id
-WHERE boards.user_id=${otherUserId ? otherUserId : userId}),'[]'))
-FROM (SELECT pin_id, pins.image FROM unboard_pin JOIN pins ON unboard_pin.pin_id = pins.id WHERE unboard_pin.user_id=${
+  (SELECT JSON_ARRAYAGG(JSON_OBJECT('id',p.pin_id,'image',p.image)) no_idea_pin FROM (SELECT pin_id, pins.image FROM unboard_pin LEFT JOIN pins ON unboard_pin.pin_id = pins.id WHERE unboard_pin.user_id=${
+    otherUserId ? otherUserId : userId
+  }) p) no_idea_pins,
+  (SELECT JSON_MERGE(IFNULL(JSON_ARRAYAGG(JSON_OBJECT('id',p.pin_id,'image',image)),'[]'), IFNULL((SELECT JSON_ARRAYAGG(JSON_OBJECT('id',id,'image',image)) ps FROM pins WHERE user_id=${
+    otherUserId ? otherUserId : userId
+  }),'[]'))
+  FROM (SELECT pin_id, pins.image FROM unboard_pin JOIN pins ON unboard_pin.pin_id = pins.id WHERE unboard_pin.user_id=${
     otherUserId ? otherUserId : userId
   }) p) all_pins,
   (SELECT JSON_ARRAYAGG(JSON_OBJECT('id',id,'image',image)) ps FROM pins WHERE user_id=${
