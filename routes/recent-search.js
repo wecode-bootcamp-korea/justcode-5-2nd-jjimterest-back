62c572b6dd01bd2dc644b43f233642ddae679e2f
azure928 <azure6.dev@gmail.com>
2022-07-21 16:16:17 +0900

    컨플릭트 해결

--- routes/recent-search.js
+++ routes/recent-search.js
@@ -6,9 +6,6 @@ const router = express.Router();
 router.use(isLogin);
 
 router.get('/recent-search', recentSearchController.serchList);
-<<<<<<< HEAD
-=======
 router.delete('/recent-search', recentSearchController.deleteAllSerchList);
->>>>>>> develop
 
 export default router;
