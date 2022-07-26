e0ae60cbae249827af289cc046854ab866c48fdf
azure928 <azure6.dev@gmail.com>
2022-07-26 16:53:37 +0900

    Refactor : 핀 정리하기 - pin-organize.js 로 파일 분리해놨던것 pin.js로 합침

--- models/pins.js
+++ models/pins.js
@@ -55,3 +55,15 @@ export const createUnboardPin = async (pinId, userId) => {
     },
   });
 };
+
+export async function organizePins(userId, pinId, boardId) {
+  return prismaClient.$transaction([
+    prismaClient.$queryRaw`
+      DELETE FROM unboard_pin 
+      WHERE pin_id = ${pinId} AND user_id = ${userId};
+    `,
+    prismaClient.$queryRaw`
+      INSERT INTO board_store (board_id, pin_id) VALUES (${boardId}, ${pinId}); 
+   `,
+  ]);
+}
--- services/pin-organize.js
+++
@@ -1,5 +0,0 @@
-import * as pinsOrganizeModels from '../models/pin-organize.js';
-
-export async function organizePins(userId, pinId, boardId) {
-  await pinsOrganizeModels.organizePins(userId, pinId, boardId);
-}
